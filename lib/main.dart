51a7e0b4790def48c3f97cdd858f5a48def3d2dd
satochibi <satochibi0120@icloud.com>
2023-05-15 11:28:44 +0900

    お絵かきアプリの最低限の機能を作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,109 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      home: DrawingPage(),
+    );
+  }
+}
+
+class DrawingPage extends StatefulWidget {
+  const DrawingPage({super.key});
+
+  @override
+  _DrawingPageState createState() => _DrawingPageState();
+}
+
+class _DrawingPageState extends State<DrawingPage> {
+  StrokesModel strokes = StrokesModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawing App'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('PictureRecorder Test'),
-        ),
-        body: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: FutureBuilder(
-            future: getPattern(),
-            builder: (BuildContext context, AsyncSnapshot<ui.Image> snapshot) {
-              return CustomPaint(
-                painter: _SamplePainter(snapshot.data),
-              );
-            },
-          ),
-        ),
+      body: FutureBuilder(
+        future: strokes.screentoneImage(),
+        builder: (context, snapshot) {
+          return SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: GestureDetector(
+              onPanDown: (details) {
+                strokes.onPress(details.localPosition);
+                setState(() {});
+              },
+              onPanUpdate: (details) {
+                strokes.drawing(details.localPosition);
+                setState(() {});
+              },
+              onPanEnd: (details) {
+                setState(() {});
+              },
+              child: CustomPaint(
+                painter: _DrawingPainter(strokes),
+              ),
+            ),
+          );
+        },
       ),
     );
+  }
+}
+
+class _DrawingPainter extends CustomPainter {
+  final StrokesModel strokes;
+
+  _DrawingPainter(this.strokes);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (strokes.all.isEmpty) return;
+    var path = Path();
+
+    strokes.all.forEach((Stroke stroke) {
+      stroke._points.asMap().forEach((int index, Offset anOffset) {
+        if (index == 0) {
+          path.moveTo(anOffset.dx, anOffset.dy);
+        } else {
+          path.lineTo(anOffset.dx, anOffset.dy);
+        }
+      });
+    });
+
+    Paint paint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..isAntiAlias = false
+      ..strokeWidth = 4.0;
+
+/*
+    if (strokes.screentoneImage != null) {
+      paint = Paint()
+        ..strokeCap = StrokeCap.round
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 4.0
+        ..isAntiAlias = false
+        ..shader = ImageShader(strokes.screentoneImage as ui.Image,
+            TileMode.repeated, TileMode.repeated, Matrix4.identity().storage);
+    }
+    */
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(_DrawingPainter oldDelegate) {
+    return true;
   }
 }
 
@@ -40,67 +111,56 @@ Future<ui.Image> getPattern() async {
   var pictureRecorder = ui.PictureRecorder();
   Canvas patternCanvas = Canvas(pictureRecorder);
 
-  const colorList = [
-    Colors.black,
-    Color(0xffff7f7f),
-    Color(0xffff7fff),
-    Color(0xff7f7fff),
-    Color(0xff7fbfff),
-    Color(0xff7fffff),
-    Color(0xff7fff7f),
-    Color(0xffffff7f),
-    Color(0xffffbf7f)
-  ];
+  final paint = Paint()
+    ..color = Colors.black
+    ..strokeWidth = 1
+    ..style = PaintingStyle.stroke
+    ..strokeJoin = StrokeJoin.round
+    ..isAntiAlias = false;
 
-  final paintList = colorList
-      .map((e) => Paint()
-        ..color = e
-        ..strokeWidth = 1
-        ..style = PaintingStyle.stroke
-        ..strokeJoin = StrokeJoin.round
-        ..isAntiAlias = false)
-      .toList();
-
-  paintList.asMap().forEach((index, element) => patternCanvas.drawPoints(
-      ui.PointMode.points, [Offset(index % 3, index / 3)], paintList[index]));
+  patternCanvas.drawPoints(
+      ui.PointMode.points, [const Offset(0, 0), const Offset(1, 1)], paint);
 
   final aPatternPicture = pictureRecorder.endRecording();
 
-  return aPatternPicture.toImage(3, 3);
+  return aPatternPicture.toImage(2, 2);
 }
 
-class _SamplePainter extends CustomPainter {
-  final ui.Image? aPattern;
+class StrokesModel {
+  List<Stroke> _strokes = [];
 
-  _SamplePainter(this.aPattern);
+  get all => _strokes;
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    if (aPattern != null) {
-      final paint = Paint()
-        ..strokeCap = StrokeCap.round
-        ..style = PaintingStyle.stroke
-        ..strokeWidth = 10
-        ..isAntiAlias = false
-        ..shader = ImageShader(aPattern!, TileMode.repeated, TileMode.repeated,
-            Matrix4.identity().storage);
-
-      var path = Path();
-      path.moveTo(size.width / 2, size.height / 5);
-      path.lineTo(size.width / 4, size.height / 5 * 4);
-      path.lineTo(size.width / 4 * 3, size.height / 5 * 4);
-      path.close();
-
-      canvas.drawPath(path, paint);
-
-      //Depending on the environment, the Offset(0, 0) point of the pattern is not displayed.
-      canvas.drawImage(
-          aPattern!, Offset(size.width / 2, size.height / 2), Paint());
+  Future<void> screentoneImage() async {
+    for (var stroke in _strokes) {
+      // stroke.screentoneImage ??= await getPattern();
     }
   }
 
-  @override
-  bool shouldRepaint(_SamplePainter oldDelegate) {
-    return aPattern != oldDelegate.aPattern;
+  void onPress(Offset offset) {
+    _strokes.add(Stroke()..add(offset));
+  }
+
+  void drawing(Offset offset) {
+    _strokes.last.add(offset);
+  }
+
+  void clear() {
+    _strokes = [];
+  }
+}
+
+class Stroke {
+  final List<Offset> _points = [];
+  ui.Image? screentoneImage;
+
+  Stroke();
+
+  add(Offset offset) {
+    _points.add(offset);
+  }
+
+  all() {
+    return _points;
   }
 }

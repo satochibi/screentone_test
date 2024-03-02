123d527ca4320bdf71d3054b374f4f37c45bb5b5
satochibi <satochibi0120@icloud.com>
2024-03-02 10:04:10 +0900

    💄 fakeウィジェットを追加

---
+++ lib/fake.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FakeDevicePixelRatio extends StatelessWidget {
+  final num fakeDevicePixelRatio;
+  final Widget child;
+
+  const FakeDevicePixelRatio(
+      {Key? key, required this.fakeDevicePixelRatio, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final devicePixelRatio = View.of(context).devicePixelRatio;
+
+    final ratio = fakeDevicePixelRatio / devicePixelRatio;
+
+    return FractionallySizedBox(
+        widthFactor: 1 / ratio,
+        heightFactor: 1 / ratio,
+        child: Transform.scale(scale: ratio, child: child));
+  }
+}
